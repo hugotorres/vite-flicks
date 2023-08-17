260ddf513bdfc3c47631cb331d7176d3e82c6872
Hugo Torres <hugo.torres@morningstar.com>
2023-08-16 19:34:52 -0500

    delete everithing

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router';
-
-new Vue({
-    router,
-    render: (h) => h(App),
-  }).$mount('#app');
