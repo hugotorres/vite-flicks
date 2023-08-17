af47dceff0801e398743a31b0b39d9090780a9ca
Hugo Torres <hugo.torres@morningstar.com>
2023-08-16 19:50:06 -0500

    with navigation

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <!-- Add your home page content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <header>
+      <h1>My Portfolio</h1>
+      <!-- Add your header content here -->
+    </header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <!-- Add more navigation links as needed -->
+    </nav>
+    <main>
+      <!-- Add your main content here -->
+      <router-view></router-view>
+    </main>
+    <footer>
+      <!-- Add your footer content here -->
+      <p>&copy; 2023 Hugo Andres. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+/* Add your custom styles for the layout here */
+header {
+  background-color: #f2f2f2;
+  padding: 10px;
+}
+
+nav {
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+}
+
+nav a {
+  color: #fff;
+  margin-right: 10px;
+  text-decoration: none;
+}
+
+main {
+  padding: 20px;
+}
+
+footer {
+  background-color: #f2f2f2;
+  padding: 10px;
+  text-align: center;
+}
+</style>
